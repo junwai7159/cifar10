f2b4e9baa20db75119883067817a99b4a2509b7d
chiang.jw <67097978+junwai7159@users.noreply.github.com>
2022-05-15 02:02:05 +0800

    Update AlexNet.py

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -1,5 +1,5 @@
 class AlexNet(nn.Module):
-    def __init__(self, num_classes=NUM_CLASSES):
+    def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
